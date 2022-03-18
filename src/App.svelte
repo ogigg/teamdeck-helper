a656a4cda6040993ba99bb58794f8bca3540b1d4
Oskar Gierszewski <oskar@moodup.team>
2022-03-18 20:36:18 +0100

    Add material ui and scss support

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,25 @@
 <script lang="ts">
   import Router, { link } from 'svelte-spa-router';
   import { routes } from './App.routes';
+  import Header from './components/header/Header.svelte';
   export let name: string;
 </script>
 
 <main>
+  <Header />
   <a href="/" use:link>Home</a>
   <a href="/settings" use:link>Settings</a>
   <Router {routes} />
 </main>
 
-<style>
+<style lang="scss">
+  body {
+    padding: 0;
+    background-color: #fafafa;
+  }
   main {
     text-align: center;
-    padding: 1em;
+    padding: 0;
     max-width: 240px;
     margin: 0 auto;
   }
